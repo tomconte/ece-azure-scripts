4d7cfd5025b7b3b16611874f9b97a86540a1ed5b
Thomas Conte <thomas@conte.com>
2018-06-14 16:24:37 +0200

    Packer/Terraform config

--- README.md
+++ README.md
@@ -1,8 +1,77 @@
 # Elastic Cloud Enterprise setup script for Microsoft Azure
 
-This is a set of scripts that will automate the creation of the Azure environment for Elastic Cloud Enterprise (ECE).
+This is a set of automation tools and scripts that will automate the creation of the Azure environment for Elastic Cloud Enterprise (ECE).
 
-Run `provision.sh` to provision three Virtual Machines ready to be used to deploy ECE, including networking, security, and OS pre-requisites. The `node_setup.sh` script will be executed on each VM in order to prepare the OS. Each VM will be rebooted for the system settings to take effect.
+## Terraform-based deployment
+
+This method uses Packer to generate a base image with all the pre-requisites, and a Terraform configuration to deploy the Azure resources. It requires to prepare the image up front, but makes the deployment itself much faster since the OS is already prepared.
+
+### Preparing the environment
+
+This method require you to first define some environment variables to authorize Packer and Terraform to access Azure.
+
+```
+export ARM_SUBSCRIPTION_ID=00000000-0000-0000-0000-000000000000
+export ARM_CLIENT_ID=00000000-0000-0000-0000-000000000000
+export ARM_CLIENT_SECRET=00000000-0000-0000-0000-000000000000
+export ARM_TENANT_ID=00000000-0000-0000-0000-000000000000
+```
+
+### Run Packer
+
+First create a Resource Group:
+
+```
+az group create -n ece-base-image -l westeurope
+```
+
+Make sure you use the same name and location as used in the Packer configuration.
+
+Then run Packer:
+
+```
+packer build ece-packer.json
+```
+
+Once the VM image has been created, retrieve its ID using the following command:
+
+```
+az vm image list -g ece-base-image
+```
+
+The ID will have this form:
+
+```
+/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/ece-base-image/providers/Microsoft.Compute/images/ece-base-image
+```
+
+### Run Terraform
+
+Copy the image ID into the `storage_image_reference` resource configuration. It should look like this:
+
+```
+  storage_image_reference {
+    id = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/ece-base-image/providers/Microsoft.Compute/images/ece-base-image"
+  }
+```
+
+Then run Terraform:
+
+```
+terraform apply
+```
+
+The Terraform configuration will execute the `disk_setup.sh` script in order to format the data disk. The script will also change the Docker configuration to point to the newly created data directory.
+
+## Script-based deployment
+
+This method uses the `az` command to provision the ECE resources in Azure. It uses base Ubuntu images and prepares them according to the ECE documentation.
+
+### Provisioning the resources
+
+Run `provision.sh` to provision three Virtual Machines ready to be used to deploy ECE, including networking, security, and OS pre-requisites. The `node_setup.sh` and `disk_setup.sh` scripts will be executed on each VM in order to prepare the OS. Each VM will be rebooted for the system settings to take effect.
+
+## Installing ECE
 
 Once the machines are prepared, you can log into them to install ECE.
 
@@ -28,6 +97,6 @@ chmod +x ./elastic-cloud-enterprise.sh
 
 Then follow the ECE instructions to install on all the hosts.
 
+## Disclaimer
 
-
-Disclaimer: this is just an example. YMMV.
+This is just an example and not officially supported by Elastic nor Microsoft.
---
+++ ece.tf
@@ -0,0 +1,169 @@
+provider "azurerm" {}
+
+resource "azurerm_resource_group" "ece" {
+  name     = "ece"
+  location = "West Europe"
+}
+
+resource "azurerm_network_security_group" "ece" {
+  name                = "nsg-ece"
+  location            = "${azurerm_resource_group.ece.location}"
+  resource_group_name = "${azurerm_resource_group.ece.name}"
+
+  security_rule {
+    name                       = "ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "https"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "12443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "ece-frontend"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "9243"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "admin-ui"
+    priority                   = 400
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "12400"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_virtual_network" "ece" {
+  name                = "vnet-ece"
+  location            = "${azurerm_resource_group.ece.location}"
+  resource_group_name = "${azurerm_resource_group.ece.name}"
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "ece" {
+  name                 = "subnet-1"
+  resource_group_name  = "${azurerm_resource_group.ece.name}"
+  virtual_network_name = "${azurerm_virtual_network.ece.name}"
+  address_prefix       = "10.0.0.0/24"
+}
+
+variable "node_count" {
+  default = 3
+}
+
+variable "ADMIN_USER" {
+  default = "azureuser"
+}
+
+variable "ADMIN_PASSWORD" {
+  default = "Password1234!"
+}
+
+resource "azurerm_public_ip" "ece" {
+  count                        = "${var.node_count}"
+  name                         = "public-ip-${format("%02d", count.index+1)}"
+  location                     = "${azurerm_resource_group.ece.location}"
+  resource_group_name          = "${azurerm_resource_group.ece.name}"
+  public_ip_address_allocation = "static"
+}
+
+resource "azurerm_network_interface" "ece" {
+  count               = "${var.node_count}"
+  name                = "nic-${format("%02d", count.index+1)}"
+  location            = "${azurerm_resource_group.ece.location}"
+  resource_group_name = "${azurerm_resource_group.ece.name}"
+
+  ip_configuration {
+    name                          = "ip-config-${format("%02d", count.index+1)}"
+    subnet_id                     = "${azurerm_subnet.ece.id}"
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id          = "${element(azurerm_public_ip.ece.*.id, count.index)}"
+  }
+}
+
+resource "azurerm_virtual_machine" "test" {
+  count                 = "${var.node_count}"
+  name                  = "vm-${format("%02d", count.index+1)}"
+  location              = "${azurerm_resource_group.ece.location}"
+  resource_group_name   = "${azurerm_resource_group.ece.name}"
+  network_interface_ids = ["${element(azurerm_network_interface.ece.*.id, count.index)}"]
+  vm_size               = "Standard_DS2_v2"
+
+  storage_image_reference {
+    id = "/subscriptions/252281c3-8a06-4af8-8f3f-d6af13e4fde3/resourceGroups/ece-base-image/providers/Microsoft.Compute/images/ece-base-image"
+  }
+
+  storage_os_disk {
+    name              = "os-disk-${format("%02d", count.index+1)}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_data_disk {
+    name              = "data-disk-${format("%02d", count.index+1)}"
+    managed_disk_type = "Premium_LRS"
+    create_option     = "Empty"
+    lun               = 0
+    disk_size_gb      = "512"
+  }
+
+  os_profile {
+    computer_name  = "ece-vm-${format("%02d", count.index+1)}"
+    admin_username = "${var.ADMIN_USER}"
+    admin_password = "${var.ADMIN_PASSWORD}"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  connection {
+    type     = "ssh"
+    host     = "${element(azurerm_public_ip.ece.*.ip_address, count.index)}"
+    user     = "${var.ADMIN_USER}"
+    password = "${var.ADMIN_PASSWORD}"
+  }
+
+  provisioner "file" {
+    source      = "disk_setup.sh"
+    destination = "/tmp/disk_setup.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/disk_setup.sh",
+      "sudo /tmp/disk_setup.sh",
+    ]
+  }
+}
+
+output "addresses" {
+  value = ["${azurerm_public_ip.ece.*.ip_address}"]
+}
